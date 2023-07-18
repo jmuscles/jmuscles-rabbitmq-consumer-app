5f0b882ce5a0c72e8eb0d98936bc73117a2e6c85
Manish Goel <manishkgoel@gmail.com>
2023-07-18 14:19:49 -0400

    working code

--- src/main/java/com/jmuscles/async/consumer/Application.java
+++ src/main/java/com/jmuscles/async/consumer/Application.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  *
  */
 @SpringBootApplication
-@EnableConfigServer
 public class Application {
 
 	public static void main(String[] args) {
