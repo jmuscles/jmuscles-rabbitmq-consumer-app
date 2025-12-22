9b10a6640ce05d4d3a77899645c8dd7b1054bb7b
Manish Goel <manishkgoel@gmail.com>
2025-12-22 00:48:16 -0500

    added open telemetry and execution call attempt

--- src/main/java/com/jmuscles/async/consumer/Application.java
+++ src/main/java/com/jmuscles/async/consumer/Application.java
@@ -3,6 +3,9 @@ package com.jmuscles.async.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+
 /**
  * Hello world!
  *
@@ -11,6 +14,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		// --- MANDATORY: SETUP REAL IDs BEFORE SPRING STARTS ---
+		SdkTracerProvider tracerProvider = SdkTracerProvider.builder().build();
+		OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).buildAndRegisterGlobal();
+		// ------------------------------------------------------
+
 		SpringApplication.run(Application.class, args);
 	}
 
