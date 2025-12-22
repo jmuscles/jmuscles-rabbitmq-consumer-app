a334dc5f12d6a4c4536d20777737a7ab813753a7
Manish Goel <manishkgoel@gmail.com>
2025-12-22 18:18:14 -0500

    removed otel dependencies and code to use otel agent

--- src/main/java/com/jmuscles/async/consumer/Application.java
+++ src/main/java/com/jmuscles/async/consumer/Application.java
@@ -3,9 +3,10 @@ package com.jmuscles.async.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
-
+*/
 /**
  * Hello world!
  *
@@ -14,10 +15,11 @@ import io.opentelemetry.sdk.trace.SdkTracerProvider;
 public class Application {
 
 	public static void main(String[] args) {
-		// --- MANDATORY: SETUP REAL IDs BEFORE SPRING STARTS ---
-		SdkTracerProvider tracerProvider = SdkTracerProvider.builder().build();
-		OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).buildAndRegisterGlobal();
-		// ------------------------------------------------------
+		/*
+		 * SdkTracerProvider tracerProvider = SdkTracerProvider.builder().build();
+		 * OpenTelemetrySdk.builder().setTracerProvider(tracerProvider).
+		 * buildAndRegisterGlobal();
+		 */
 
 		SpringApplication.run(Application.class, args);
 	}
