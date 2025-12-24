b3af03e5368293b375b12e39210f5a800acad6f3
Manish Goel <manishkgoel@gmail.com>
2025-12-23 21:44:25 -0500

    Spring boot upgrade

--- src/main/java/com/jmuscles/async/consumer/MetricsGenerator.java
+++ src/main/java/com/jmuscles/async/consumer/MetricsGenerator.java
@@ -4,7 +4,7 @@
 /*
  * package com.jmuscles.async.consumer;
  * 
- * import javax.annotation.PostConstruct;
+ * import jakarta.annotation.PostConstruct;
  * 
  * import org.slf4j.Logger; import org.slf4j.LoggerFactory; import
  * org.springframework.beans.factory.annotation.Value; import
